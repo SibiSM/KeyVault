323f0b0acbd465bc761a09d8ee06de1a5cef856e
SibiSM <sibichakravarthy888@gmail.com>
2024-10-11 14:21:19 -0400

    Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { DefaultAzureCredential } = require('@azure/identity');
+const { SecretClient } = require('@azure/keyvault-secrets');
+
+// Set up Express app
+const app = express();
+const port = 3000;
+
+// Azure Key Vault details
+const keyVaultName = process.env.KEY_VAULT_NAME; // Get from environment variable
+const KVUri = `https://${keyVaultName}.vault.azure.net`;
+
+// Use DefaultAzureCredential to authenticate
+const credential = new DefaultAzureCredential();
+const client = new SecretClient(KVUri, credential);
+
+app.get('/', async (req, res) => {
+    try {
+        // Fetch secret from Azure Key Vault
+        const secretName = process.env.SECRET_NAME; // Get from environment variable
+        const retrievedSecret = await client.getSecret(secretName);
+        
+        // Return the secret value
+        res.send(`The secret value is: ${retrievedSecret.value}`);
+    } catch (err) {
+        console.error('Error retrieving secret:', err);
+        res.status(500).send('Error retrieving secret');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
